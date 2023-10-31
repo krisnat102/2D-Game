7fb05e43ce3a53c9506a865f33903cf1d639a764
Christopher <alhokolik102@gmail.com>
2023-10-31 03:06:09 +0200

    Platforms
    Added a script that turns any solid object into a platform (you can pass through the bottom side but the top side is solid)

---
+++ 2D Game/Assets/Scripts/Platform.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    Collider2D platform;
+
+    [SerializeField] private Transform player;
+
+    [SerializeField] float offset = 1f;
+
+    private void Start()
+    {
+        platform = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if (player.position.y < transform.position.y + offset)
+        {
+            platform.enabled = false;
+        }
+        else
+        {
+            platform.enabled = true;
+        }
+    }
+}
