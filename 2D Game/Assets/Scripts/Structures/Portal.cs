d18ee4bffb35a62b6e88a61ff067a6af1c6f466a
Christopher <alhokolik102@gmail.com>
2024-10-08 21:46:38 +0300

    Tutorial
    Small work on the tutorial

--- 2D Game/Assets/Scripts/Structures/Portal.cs
+++ 2D Game/Assets/Scripts/Structures/Portal.cs
@@ -9,10 +9,16 @@ namespace Krisnat
     public class Portal : MonoBehaviour, IStructurable
     {
         [SerializeField] private string sceneToLoad;
+        [SerializeField] private Vector3 distanceTravel;
         public void OnTriggerStay2D(Collider2D collision)
         {
             if (PlayerInputHandler.Instance.UseInput)
             {
+                if(sceneToLoad == "" && distanceTravel != Vector3.zero)
+                {
+                    collision.transform.position += distanceTravel;
+                    return;
+                }
                 //if(sceneToLoad == "MainMenu") collision.GetComponent<Player>().gameObject.SetActive(false);
                 UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
 
