8b07d18604f9986dd38bcccd3b8c6da26c847ae6
Christopher <alhokolik102@gmail.com>
2024-12-23 03:26:49 +0200

    Bonfire
    Added a bonfire to the game

--- 2D Game/Assets/Scripts/Structures/LevelUpStation.cs
+++ 2D Game/Assets/Scripts/Structures/LevelUpStation.cs
@@ -6,23 +6,36 @@ namespace Krisnat
     {
         [SerializeField] private float openingAnimationDuration = 0.2f;
         [SerializeField] private float closingAnimationDuration = 0.2f;
+        [SerializeField] private GameObject fire;
+        private bool triggered;
+
         public void OnTriggerStay2D(Collider2D collision)
         {
             var player = collision.GetComponent<Player>();
             var levelUpUI = UIManager.Instance.LevelUpInterface;
             var scale = levelUpUI.transform.localScale.x;
+
             if (PlayerInputHandler.Instance.UseInput && player != null)
             {
-                if (!UIManager.Instance.LevelUpInterface.activeInHierarchy)
+                if (triggered)
                 {
-                    levelUpUI.SetActive(true);
-                    levelUpUI.transform.localScale = new Vector3(0.05f, 0.05f, levelUpUI.transform.localScale.z);
-                    UIManager.Instance.OpenCloseUIAnimation(levelUpUI, scale, openingAnimationDuration, true);
+                    if (!UIManager.Instance.LevelUpInterface.activeInHierarchy)
+                    {
+                        levelUpUI.SetActive(true);
+                        levelUpUI.transform.localScale = new Vector3(0.05f, 0.05f, levelUpUI.transform.localScale.z);
+                        UIManager.Instance.OpenCloseUIAnimation(levelUpUI, scale, openingAnimationDuration, true);
+                    }
+                    else
+                    {
+                        UIManager.Instance.OpenCloseUIAnimation(levelUpUI, 0.05f, closingAnimationDuration, false);
+                    }
                 }
                 else
                 {
-                    UIManager.Instance.OpenCloseUIAnimation(levelUpUI, 0.05f, closingAnimationDuration, false);
+                    fire.SetActive(true);
+                    triggered = true;
                 }
+
                 PlayerInputHandler.Instance.UseUseInput();
             }
         }
